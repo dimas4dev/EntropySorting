9d2a129c5538d8c32fbce383bd73637118bf6947
Dimas Mendoza <damendoza98@gmail.com>
2020-12-17 19:24:26 -0500

    Creacion del proyecto del Hackaton

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hola Mundo</h1>
+      </Fragment>
+    );
+  }
+}
+export default App;
