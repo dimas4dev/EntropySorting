3f0178d9f3cd495ca3dcf28a763546a39aaba153
Dimas Mendoza <damendoza98@gmail.com>
2020-12-18 23:58:35 -0500

    Se añade algo para sobrevivir

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -1,23 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../assets/styles/JobCard/JobCard.scss";
 import Relojito from "../../assets/images/clock.svg";
 import IconJobsito from "../../assets/images/jobs.svg";
 import Locacioncita from "../../assets/images/marcador-de-posicion.svg";
 
 const JobCard = () => {
+  const [jobCards, setJobCards] = useState([]);
+  const [quere, setQuery] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3000/areas")
+      .then((response) => response.json())
+      .then((data) => setJobCards(data[0].DS[0]));
+  });
   return (
     <article className="Card">
       <div className="Card__date">
         <img src={Relojito} alt="" />
-        <p>17/06/2020</p>
+        <p>{jobCards.fecha}</p>
       </div>
       <div className="Card__jobname">
         <img src={IconJobsito} alt="" />
-        <p>Principal Frontend Enginner, Prodigious</p>
+        <p>{jobCards.nombres}</p>
       </div>
       <div className="Card__jobcity">
         <img src={Locacioncita} alt="" />
-        <p>Colombia, English Required</p>
+        <p>{jobCards.ciudad}</p>
       </div>
       <div className="Card__button">
         <div>Apply Now</div>
