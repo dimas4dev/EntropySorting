156a2ac6f3ecf9948e2346e55026ab009c28ca57
Dimas Mendoza <damendoza98@gmail.com>
2020-12-19 01:07:08 -0500

    Envio Cambios

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -6,18 +6,39 @@ import Locacioncita from "../../assets/images/marcador-de-posicion.svg";
 
 const JobCard = () => {
   const [jobCards, setJobCards] = useState([]);
-  const [quere, setQuery] = useState("");
-
+  const [Query, setQuery] = useState("");
+  // const { title } = variable;
   useEffect(() => {
-    fetch("http://localhost:3000/areas")
-      .then((response) => response.json())
-      .then((data) => setJobCards(data[0].DS[0]));
+    // fetch("http://localhost:3000/areas")
+    //   .then((response) => response.json())
+    //   .then((data) => setJobCards(data[0]));
+    fetch("https://lootvoid.xyz", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query: `
+    {
+      getVacants(carrer: Frontend, page: 1) {
+        link,
+        title
+      }
+    }`,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => setJobCards(data.data.getVacants));
   });
   return (
     <article className="Card">
       <div className="Card__date">
         <img src={Relojito} alt="" />
-        <p>{jobCards.fecha}</p>
+        <p>
+          {jobCards.length > 0
+            ? jobCards.map(() => {
+                jobCards[0].link;
+              })
+            : "none"}
+        </p>
       </div>
       <div className="Card__jobname">
         <img src={IconJobsito} alt="" />
